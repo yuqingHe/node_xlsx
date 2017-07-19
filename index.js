c0af16f9f234db82338fbdf5239c04b10fb21b44
wb-hyq283934 <wb-hyq283934@antfin.com>
2017-07-19 16:09:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+// https://scarletsky.github.io/2016/01/30/nodejs-process-excel/
+const  XLSX = require('xlsx');
+const utils = require('./utils');
+
+
+// const sourceData = utils.getJsonByPath('./output.xlsx');
+// const dataBase = utils.getJsonByPath('./output.xlsx');
+
+
+
+const header=['姓名','年龄'];
+const data=[{
+  '姓名':123,
+  '年龄':43
+}];
+utils.expertFile(header, data);
+
+console.log('===执行成功======');
